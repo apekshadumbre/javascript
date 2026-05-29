c558964a2a1411eecb336de417eb1c552c669908
Apeksha <apekshadumbre162@gmail.com>
2026-05-29 13:27:17 +0530

    modified student data logic

--- student_data.js
+++ student_data.js
@@ -4,6 +4,21 @@ function saveStudentData()
 {
     let stud_name=document.getElementById("fname").value; 
         localStorage.setItem("fname",stud_name);
+
     document.getElementById("stud_Name").innerHTML ="Student Name:" +
         localStorage.getItem("fname");
+
+        let stud_mail=document.getElementById("gmail").value;
+        localStorage.setItem("gmail",stud_mail);
+
+        document.getElementById("stud_mail").innerHTML="student mail:"+
+        localStorage.getItem("gmail");
+
+        let stud_status=document.getElementById("status").value;
+        localStorage.setItem("as",stud_status);
+
+       document.getElementById("stud_status").innerHTML="Student Status:"+
+        localStorage.getItem("as");
+           
+    
 }
